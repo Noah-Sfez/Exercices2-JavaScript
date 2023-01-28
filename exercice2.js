2f88b75258efc408f6afb0bddc235e2b3d7e5077
Noah-Sfez <116373700+Noah-Sfez@users.noreply.github.com>
2023-01-28 15:48:48 +0100

    Add files via upload

---
+++ exercice2.js
@@ -0,0 +1,7 @@
+//document.getElementById("button").onclick(document.getElementById("fra").innerHTML="Portugal");
+//document.getElementById("button-reverse").onclick(document.getElementById("fra").innerHTML="France");
+
+
+var pays = document.getElementById("pays");
+
+pays.insertAdjacentHTML('afterend', '<li>italie<li>');
